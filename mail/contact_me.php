203ebce7fc89464428ec8a633f696dd58ca8a220
Emilio Ochoa <emilioaor@gmail.com>
2017-10-26 22:52:16 -0400

    add PHPMailer

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,22 +9,44 @@ if(empty($_POST['name'])  		||
    		$_SESSION['alert-msg'] = 'Disculpe, faltan datos';
    		$_SESSION['alert-type'] = 'alert-danger';
 		echo "No arguments Provided!";
-		echo "<script type='text/javascript'>location.href='../index.php'</script>";
+		echo "<script type='text/javascript'>location.href='../'</script>";
 		die;
    }
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'emilioaor@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Mi portafolio - Contactos:  $name";
-$email_body = "Has recibido un mensaje desde la página de contactos.\n\n"."He aquí los detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nMensaje:\n$message";
-$headers = "Desde: noreply@ddg.com.ve\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Copia: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+
+require 'PHPMailer/class.phpmailer.php';
+
+$mail = new PHPMailer(true); 
+ 
+// Configuramos el protocolo SMTP con autenticación
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+ 
+// Configuración del servidor SMTP
+$mail->Port = 465;
+$mail->Host = 'smtp.gmail.com';
+$mail->Username   = "emilioaor@gmail.com";
+$mail->Password = "emi21029522lio";
+$mail->SMTPAuth   = true;
+// Configuración cabeceras del mensaje
+$mail->From = "emilioaor@gmail.com";
+$mail->FromName = "Mi portafolio";
+ 
+$mail->AddAddress("emilioaor@gmail.com");
+$mail->Subject = "Mi portafolio - Contactos:  $name";
+ 
+// Creamos en una variable el cuerpo, contenido HMTL, del correo
+$body  = "Has recibido un mensaje desde la página de contactos.\n\n"."He aquí los detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nMensaje:\n$message";
+ 
+$mail->Body = $body;
+ 
+// Enviar el correo 
+$mail->Send(); 
+
 //Alert
 $_SESSION['alert-msg'] = 'Su mensaje fue enviado satisfactoriamente';
 $_SESSION['alert-type'] = 'alert-success';
-echo "<script type='text/javascript'>location.href='../index.php'</script>";
+echo "<script type='text/javascript'>location.href='../'</script>";
